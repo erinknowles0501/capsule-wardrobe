3e7480c06ca3615f527fd5c7bc8e2143368a88fb
Erin <colorandcontrast@gmail.com>
2020-07-20 20:38:23 -0300

    Implement draggable in capsule.

--- src/views/MyCapsule.vue
+++ src/views/MyCapsule.vue
@@ -1,16 +1,79 @@
 <template>
   <v-container>
     <h1>My Capsule</h1>
-    <v-sheet depressed class="display closet" v-if="!loading">
-      <v-card outlined v-for="(item, index) in closet" :key="index">
-        <v-card-title class="subtitle-2">{{ item.name }}</v-card-title>
-      </v-card>
+    <v-sheet depressed v-if="!loading">
+      <draggable
+        :list="closet"
+        class="draggable draggable-closet display display-closet"
+        ghost-class="draggable-ghost"
+        :group="{ name: 'capsule', pull: 'clone', put: false }"
+      >
+        <v-card
+          outlined
+          v-for="(item, index) in closet"
+          :key="index"
+          :disabled="isUsed(item)"
+          :class="{ 'draggable-ghost': isUsed(item) }"
+        >
+          <v-card-title class="subtitle-2">{{ item.name }}</v-card-title>
+        </v-card>
+      </draggable>
     </v-sheet>
 
-    <v-sheet depressed class="display winter">
-      <v-card flat v-for="(item, index) in capsule.winter" :key="index">
-        <v-card-title class="subtitle-2">{{ item.name }}</v-card-title>
-      </v-card>
+    <v-sheet depressed>
+      <draggable
+        :list="capsule.winter"
+        class="draggable draggable-winter display display-winter"
+        ghost-class="draggable-ghost"
+        group="capsule"
+        @end="checkDrop"
+      >
+        <v-card flat v-for="(item, index) in capsule.winter" :key="index">
+          <v-card-title class="subtitle-2">{{ item.name }}</v-card-title>
+        </v-card>
+      </draggable>
+    </v-sheet>
+
+    <v-sheet depressed>
+      <draggable
+        :list="capsule.spring"
+        class="draggable draggable-spring display display-spring"
+        ghost-class="draggable-ghost"
+        group="capsule"
+        @end="checkDrop"
+      >
+        <v-card flat v-for="(item, index) in capsule.spring" :key="index">
+          <v-card-title class="subtitle-2">{{ item.name }}</v-card-title>
+        </v-card>
+      </draggable>
+    </v-sheet>
+
+    <v-sheet depressed>
+      <draggable
+        :list="capsule.summer"
+        class="draggable draggable-summer display display-summer"
+        ghost-class="draggable-ghost"
+        group="capsule"
+        @end="checkDrop"
+      >
+        <v-card flat v-for="(item, index) in capsule.summer" :key="index">
+          <v-card-title class="subtitle-2">{{ item.name }}</v-card-title>
+        </v-card>
+      </draggable>
+    </v-sheet>
+
+    <v-sheet depressed>
+      <draggable
+        :list="capsule.fall"
+        class="draggable draggable-fall display display-fall"
+        ghost-class="draggable-ghost"
+        group="capsule"
+        @end="checkDrop"
+      >
+        <v-card flat v-for="(item, index) in capsule.fall" :key="index">
+          <v-card-title class="subtitle-2">{{ item.name }}</v-card-title>
+        </v-card>
+      </draggable>
     </v-sheet>
   </v-container>
 </template>
@@ -23,19 +86,26 @@
  *    - On main view page - items not in the capsule are tinted red
  *
  *
+ * NOTE: :remove-on-spill should have worked EXCEPT it wasn't removing the items from the list. Known bug. Keep an eye out for it being fixed.
+ *
  * */
 import db from "@/firebase/init";
+import draggable from "vuedraggable";
 
 export default {
+  components: {
+    draggable,
+  },
   data() {
     return {
       loading: true,
+      dragging: false,
 
       capsule: {
         spring: [],
         summer: [],
         fall: [],
-        winter: [{ name: "dsgsdg" }],
+        winter: [],
         base: [],
       },
       closet: [],
@@ -55,6 +125,21 @@ export default {
 
     this.loading = false;
   },
+  methods: {
+    isUsed(item) {
+      return (
+        this.capsule.winter.includes(item) ||
+        this.capsule.summer.includes(item) ||
+        this.capsule.fall.includes(item) ||
+        this.capsule.spring.includes(item)
+      );
+    },
+    checkDrop(e) {
+      if (!e.pullMode) {
+        console.log("%c SAME", "color: red");
+      }
+    },
+  },
 };
 </script>
 
@@ -64,12 +149,29 @@ export default {
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   grid-gap: 0.5em;
   padding: 0.5em;
+  min-height: 50px;
 }
 
-.winter {
-}
-
-.winter {
+.display-winter {
   background: lightblue;
+}
+
+.display-spring {
+  background: lightgreen;
+}
+
+.display-summer {
+  background: lightseagreen;
+}
+
+.display-fall {
+  background: lightsalmon;
+}
+
+.display-closet {
+}
+
+.draggable-ghost {
+  filter: blur(1px) opacity(75%);
 }
 </style>
