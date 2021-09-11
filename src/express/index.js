c957ddb7b615833650c1e6ffe244931a58cb7eb2
Erin <colorandcontrast@gmail.com>
2021-09-11 04:01:14 -0700

    Fix tying together backend (using node /express/index.js etc to troubleshoot). Remove deprecated body-parser and replace.

--- src/express/index.js
+++ src/express/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-import db from "@/mongoose";
+const db = require("../mongoose");
 
 db.once("open", (_) => {
   console.log("Database connected");
@@ -15,32 +14,33 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get(/.*/, (req, res) =>
-  res.sendFile(path.resolve(__dirname, "public/index.html"))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
 );
 
 // simple route
-// app.get("/test", (req, res) => {
-//   res.json({ message: "Welcome to application." });
-//   //   const newItem = new ItemModel({
-//   //     name: "sdgsdgsdg",
-//   //     description: "msdfsdfsdfale",
-//   //   });
-//   //   newItem.save(function(error, document) {
-//   //     if (error) console.error(error);
-//   //     console.log(document);
-//   //   });
-//   //   res.json(newItem);
-// });
+app.get("/test", (req, res) => {
+  res.json({ message: "Welcome to application." });
+  //   const newItem = new ItemModel({
+  //     name: "sdgsdgsdg",
+  //     description: "msdfsdfsdfale",
+  //   });
+  //   newItem.save(function(error, document) {
+  //     if (error) console.error(error);
+  //     console.log(document);
+  //   });
+  //   res.json(newItem);
+});
+
+// app.get(/.*/, (req, res) =>
+//   res.sendFile(path.resolve(__dirname, "public/index.html"))
+// );
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
